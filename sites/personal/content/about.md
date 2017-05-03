e48884285f0f0006c76aad50fb5b97ded514797e
Evan Klemen <evan.klemen@wbdcorp.com>
2017-05-03 10:56:15 -0400

    moved to subdir for multisite

--- sites/personal/content/about.md
+++ sites/personal/content/about.md
@@ -2,7 +2,7 @@
 date = "2017-05-02T13:56:42-04:00"
 draft = false
 title = "about"
-weight = 0
+weight = 1
 
 +++
 
---
+++ sites/personal/static/js/custom.js
@@ -0,0 +1,19 @@
+$(document).ready(function($) { 
+    $(function(){
+        if (window.location.search == "?sent") {
+            $('#contactform').hide();
+            $('#contactformsent').show();
+        } else {
+            $('#contactformsent').hide();
+        }
+    });
+    
+    $('.reveal').on('click', function(){
+        if ( $(this).next('.slide-content.hidden').is(":hidden") ) {
+            $(".slide-content.hidden").slideDown("fast");
+        } else {
+            $('.slide-content').slideUp("fast");
+        }
+    })
+    // console.log(rev)
+});
